1bc0e4d3305bc2317b7650b9a646ddba6462a90f
Jamie Dawson <jadawson@e1z3r4p23.42.us.org>
2018-11-10 20:41:58 -0800

    normed and works. I need to apply this code to my push_swap project

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,14 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 16:06:34 by jadawson          #+#    #+#             */
-/*   Updated: 2018/11/10 13:36:55 by jadawson         ###   ########.fr       */
+/*   Updated: 2018/11/10 20:41:16 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*connect(char *str, char *holder) //connects str to the buf
+char	*connect(char *str, char *holder)
 {
-	printf("connect statement\n");
 	char *strptr;
 
 	strptr = ft_strjoin(str, holder);
@@ -22,11 +21,10 @@ char	*connect(char *str, char *holder) //connects str to the buf
 	return (strptr);
 }
 
-int		the_end(char **line, char **strings, char **holder) //last section we gotta deal withm.
+int		the_end(char **line, char **strings, char **holder)
 {
 	if (ft_strlen(strings[0]) != 0)
 	{
-		printf("the_end if statement\n");
 		*line = strings[0];
 		free(*holder);
 		*holder = ft_strnew(0);
@@ -42,7 +40,7 @@ int		the_end(char **line, char **strings, char **holder) //last section we gotta
 int		looping(int fd, char **strings, char **holder, char **line)
 {
 	int		ret;
-	char	buf[BUFF_SIZE + 1]; //allocate space for buff.
+	char	buf[BUFF_SIZE + 1];
 
 	while (1)
 	{
@@ -55,22 +53,26 @@ int		looping(int fd, char **strings, char **holder, char **line)
 		strings[1] = ft_strchr(buf, '\n');
 		if (strings[1] != NULL)
 		{
-			printf("looping if statement\n");
 			*strings[1] = '\0';
 			strings[0] = connect(strings[0], buf);
 			*strings[1] = '\n';
 			*holder = connect(*holder, strings[1] + 1);
 			*line = strings[0];
-			printf("holder in looping %p\n", *holder);
-			printf("holder again \"%s\"\n", *holder);
 			return (1);
 		}
-		free(strings[1]); //testing. did not work.
 		strings[0] = connect(strings[0], buf);
 	}
 }
 
-int		get_next_line(const int fd, char **line) //start
+void	holder_test(char **strings, char **holder)
+{
+	if (*holder[0] != '\n')
+		strings[0] = connect(strings[0], *holder);
+	else
+		free(strings[0]);
+}
+
+int		get_next_line(const int fd, char **line)
 {
 	char		*strings[3];
 	static char	*holder;
@@ -87,20 +89,13 @@ int		get_next_line(const int fd, char **line) //start
 		*line = ft_strdup(holder);
 		*strings[1] = '\n';
 		strings[2] = ft_strdup(strings[1] + 1);
-		printf("this line \"%s\"", strings[2]);//test
-		if (holder)
-			free(holder);
+		free(holder);
 		holder = strings[2];
 		free(strings[0]);
 		return (1);
 	}
-	if (holder[0] != '\n')
-		strings[0] = connect(strings[0], holder);//leaks if it's a \n
-	else
-		free(strings[0]);
-	printf("strings[0] after gnl if statement %p\n", strings[0]);
-	if (holder)	
-		free(holder);
+	holder_test(strings, &holder);
+	free(holder);
 	holder = ft_strnew(0);
 	return (looping(fd, strings, &holder, line));
 }
